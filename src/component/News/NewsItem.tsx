651efd693a4097db3505bf268bd7697f57983dcb
megapihar6 <karvasarniy.sa@intechs.by>
2025-06-25 20:55:32 +0200

    add start

--- src/component/News/NewsItem.tsx
+++ src/component/News/NewsItem.tsx
@@ -22,7 +22,9 @@ export const NewsItem: React.FC<Props> = ({ item }) => {
         {title}
       </h5>
       <p className="mt-[4px] text-lg font-light">{date}</p>
-      <p className="mt-5 text-base leading-[110.00000000000001%]">{text}</p>
+      <p className="mt-5 text-base leading-[110.00000000000001%] font-normal">
+        {text}
+      </p>
       <button
         type="submit"
         className="mt-[20px] flex w-[8.313rem] cursor-pointer items-center justify-between border-1 border-white py-[0.438rem] pr-4 pl-[0.625rem] uppercase transition-all duration-400 hover:opacity-[0.5]"
